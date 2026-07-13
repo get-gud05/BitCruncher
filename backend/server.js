f77b1a5c2408b4c9e79d7350d90b27deaf96626e
Akshat Dangwal <akshatdangwal07@gmail.com~>
2026-07-14 01:01:17 +0530

    minor changes to server.js

--- backend/server.js
+++ backend/server.js
@@ -283,8 +283,8 @@ app.post(
   }
 );
 
-app.listen(5000, () => {
-  console.log(
-    "Server running on port 5000"
-  );
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
